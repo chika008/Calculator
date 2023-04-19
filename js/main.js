7c316751921b93797124f850c679e9bcc85eee92
TRETYAKweb <tretyakbro@gmail.com>
2023-04-19 21:36:02 +0300

    feat(js): add calculator and dark theme toggle
    Adds a simple calculator with addition, subtraction, multiplication, division and clear functions. Also adds a toggle button to switch between dark and light themes.
    
    The dark theme toggle updates the styles of various elements including the calculator container, result area, values area, and buttons. The color of certain buttons change from blue to red while others change from light to dark.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
 const values = document.querySelector("#values");
 const result = document.querySelector("#result");
-
 const buttons = document.querySelectorAll(".btn");
+const darthVader = document.querySelector("#dark-btn");
+const yoda = document.querySelector("#light-theme");
+const calc = document.querySelector(".calc");
+const calcResult = document.querySelector("#calc-result");
+
+const calculate = (input) => {
+  return Function(`'use strict'; return (${input})`)();
+};
+
+const toggleDarkTheme = () => {
+  calc.classList.toggle("calc--dark");
+  calcResult.classList.toggle("calc__result--dark");
+  values.classList.toggle("calc__values--dark");
+
+  buttons.forEach((el) => {
+    if (el.id === "clear" || el.id === "backspace") {
+      el.classList.toggle("btn--default-color-dark");
+    } else if (["/", "*", "-", "+"].includes(el.id)) {
+      el.classList.toggle("btn--bg-red-200");
+      el.classList.toggle("btn--bg-blue-200");
+    } else if (el.id === "equal") {
+      el.classList.toggle("equal--dark");
+    } else {
+      el.classList.toggle("btn--dark");
+    }
+  });
+};
 
 buttons.forEach((element) => {
   element.onclick = () => {
@@ -11,70 +37,14 @@ buttons.forEach((element) => {
     } else if (element.id === "backspace") {
       let string = values.innerHTML.toString();
       values.innerHTML = string.slice(0, -1);
-    } else if (element.id != "" && element.id != "equal") {
+    } else if (element.id && element.id !== "equal") {
       values.innerHTML += element.id;
     } else if (element.id === "equal") {
-      let lol = eval(values.innerHTML);
+      let lol = calculate(values.innerHTML);
       result.innerHTML = lol;
     }
-    console.log(element.id);
   };
 });
 
-/* dark-theme */
-
-const darthVader = document.querySelector("#dark-btn");
-const yoda = document.querySelector("#light-theme");
-
-const calc = document.querySelector(".calc");
-const calcResult = document.querySelector("#calc-result");
-
-darthVader.addEventListener("click", (event) => {
-  darthVader.style.display = "none";
-  yoda.style.display = "block";
-  calc.classList.add("calc--dark");
-  calcResult.classList.add("calc__result--dark");
-  values.classList.add("calc__values--dark");
-  buttons.forEach((el) => {
-    if (el.id === "clear" || el.id === "backspace") {
-      el.classList.add("btn--default-color-dark");
-    } else if (
-      el.id === "/" ||
-      el.id === "*" ||
-      el.id === "-" ||
-      el.id === "+"
-    ) {
-      el.classList.add("btn--bg-red-200");
-      el.classList.remove("btn--bg-blue-200");
-    } else if (el.id === "equal") {
-      el.classList.add("equal--dark");
-    } else {
-      el.classList.add("btn--dark");
-    }
-  });
-});
-
-yoda.addEventListener("click", (event) => {
-  darthVader.style.display = "block";
-  yoda.style.display = "none";
-  calc.classList.remove("calc--dark");
-  calcResult.classList.remove("calc__result--dark");
-  values.classList.remove("calc__values--dark");
-  buttons.forEach((el) => {
-    if (el.id === "clear" || el.id === "backspace") {
-      el.classList.remove("btn--default-color-dark");
-    } else if (
-      el.id === "/" ||
-      el.id === "*" ||
-      el.id === "-" ||
-      el.id === "+"
-    ) {
-      el.classList.remove("btn--bg-red-200");
-      el.classList.add("btn--bg-blue-200");
-    } else if (el.id === "equal") {
-      el.classList.remove("equal--dark");
-    } else {
-      el.classList.remove("btn--dark");
-    }
-  });
-});
+darthVader.addEventListener("click", toggleDarkTheme);
+yoda.addEventListener("click", toggleDarkTheme);
